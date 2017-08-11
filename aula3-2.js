caf21c70e72ccb7522000324a0c6135f82e7708f
Flavio <4411132+stribus@users.noreply.github.com>
2017-08-11 20:02:48 -0300

    case insencitive

--- aula3-2.js
+++ aula3-2.js
@@ -2,3 +2,5 @@ db.pets.find({},{nome:true,_id:false})
 db.pets.find({idade:{$gte:3},proprietario:{$regex:"^[^m]"}},{nome:true,_id:false,proprietario:true})
 db.pets.find({nome:{$regex:"d"}},{nome:true,_id:false,tipo:true})
 db.pets.find({nome:{$regex:"y$"}},{nome:true,_id:false})
+//para case insencitive 
+db.pets.find({tipo:/^CacHorrO$/i},{nome:true,_id:false,tipo:true})

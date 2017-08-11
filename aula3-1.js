cf9886198e7f9b2dcc66e26893dfc513c78c733f
Flavio <4411132+stribus@users.noreply.github.com>
2017-08-11 19:30:02 -0300

    1

---
+++ aula3-1.js
@@ -0,0 +1,11 @@
+use petshop
+db.pets.insert({nome:'billy',idade:3,tipo:'cachorro',proprietario:'mario'});
+db.pets.insertMany([
+    {nome:'mel',idade:2,tipo:'gato',proprietario:'maria'},
+    {nome:'fido',idade:3,tipo:'cachorro',proprietario:'jose'},
+    {nome:'chester',idade:3,tipo:'gato',proprietario:'carime'},
+    {nome:'pity',idade:3,tipo:'cachorro',proprietario:'mario'}
+    ]);
+
+db.pets.find();
+db.pets.find({proprietario:'mario'})

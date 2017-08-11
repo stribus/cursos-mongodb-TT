0ec434b48ed4574829ca8e7b52cfb81b865b21ea
Flavio <Flavio@localhost>
2017-08-11 20:52:53 -0300

    pesquisas "avan?adas"

---
+++ aula3-4.js
@@ -0,0 +1,16 @@
+var bixos = db.pets.find();
+while( bixos.hasNext()){
+    print(bixos.next());
+}   
+var animais = db.pets.find();
+while( animais.hasNext()){
+    print(tojson(animais.next()));
+}
+
+db.pets.find().forEach(function(bixo){
+    print(tojson(bixo))
+})
+
+db.pets.find().map(function(bixo){
+    print(bixo)
+})

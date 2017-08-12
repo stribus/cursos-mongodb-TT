f50994238cddb6f885a0baa2bd1969a16933aa33
Flavio <Flavio@localhost>
2017-08-11 21:22:41 -0300

    update

--- aula3-6.js
+++ aula3-6.js
@@ -1,3 +1,15 @@
 var pet = db.pets.findOne({proprietario:'carime'})
 pet.idade= 8
-db.pets.save(pet)
+db.pets.save(pet)
+
+
+db.pets.update(
+db.pets.findOne({proprietario:'carime',idade:{$lt:4}},{_id:1})
+,{nome:'urso',idade:5,tipo:'cachorro'})
+
+
+db.pets.update(
+    db.pets.findOne({nome:'urso',idade:5},{_id:1})
+,{$set:{proprietario:'carime',tipo:'cachorro'}})
+
+

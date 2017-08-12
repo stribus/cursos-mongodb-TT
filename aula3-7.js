781693bd7bccf1ba1cb539f77acd218541c58562
Flavio <4411132+stribus@users.noreply.github.com>
2017-08-11 21:36:25 -0300

    agrega??o

---
+++ aula3-7.js
@@ -0,0 +1,12 @@
+db.pets.aggregate({
+    $match:{
+        idade: 3
+    }
+})
+db.pets.aggregate({
+    $project:{
+        nome:true,
+        idade:true,
+        _id:false
+    }
+})

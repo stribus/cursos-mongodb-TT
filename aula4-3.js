9add7f70b5dfd4ea8ce2a1534bb5b95f4b6394d2
Flavio <4411132+stribus@users.noreply.github.com>
2017-08-14 19:52:45 -0300

    index

---
+++ aula4-3.js
@@ -0,0 +1,11 @@
+db.cidade.insertMany([
+{cidade:"São Paulo"},
+{cidade:"So Paulo"},
+{cidade:"sao Paulo"},
+{cidade:"São PAULO"},
+{cidade:"SAO paulo"},
+])
+
+db.cidade.createIndex({cidade:'text'},{default_language:"portuguese"})
+
+db.cidade.find()

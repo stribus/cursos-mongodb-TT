a16b5fe184eeaf2691499f33aae5d7b14d7b0bee
Flavio <4411132+stribus@users.noreply.github.com>
2017-08-14 19:38:42 -0300

    agrega??o

---
+++ aula4-1.js
@@ -0,0 +1,26 @@
+use teste
+db.inventory.insertMany([
+{_id:1,item:"ABC", Quantidade: 158, descricao:'produto 1'},
+{_id:2,item:"abc2", Quantidade: 258, descricao:'produto 2'},
+{_id:3,item:"ABc3", Quantidade: 3245464, descricao:'produto 1'},
+{_id:4,item:"XYZ", Quantidade: 9548, descricao:'produto 2'}
+])
+
+
+db.inventory.aggregate([
+{
+    $project:{
+        item:{$toUpper:"$item"},
+        descricao:{$toUpper:"$descricao"}
+        },
+        
+        
+    }
+])
+    
+db.inventory.find().forEach(function(doc){
+    doc.item=doc.item.toUpperCase();
+    db.inventory.save(doc);
+    })    
+    
+    
